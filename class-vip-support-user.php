01495be0d12fe95087d7e183f53ea8703a96842a
Simon Wheatley <simon@simonwheatley.co.uk>
2015-04-24 10:50:21 +0100

    Verify a A12n user's email address

--- class-vip-support-user.php
+++ class-vip-support-user.php
@@ -2,8 +2,11 @@
 
 /**
  * @TODO: Verify the A12n's email on registration
+ * @TODO: The email to be verified should be hashed with a nonce or with the verification code, so the verification is tied to the specific email address
+ * @TODO: We should do a nonce check when the email is verified, so it's only the logged in user who can verify the email
  * @TODO: Verify the A12n's email when it changes, even if previous email was not A8c
  * @TODO: Block logging in until the user has verified their email address
+ * @TODO: Show a tick or something when a user's email has been verified
  *
  * @package VipSupportUser
  **/
@@ -15,6 +18,10 @@ class VipSupportUser {
 	const MSG_MADE_VIP                 = 'vip_made_vip';
 
 	const META_VERIFICATION_CODE = 'vip_email_verification_code';
+	const META_EMAIL_VERIFIED    = 'vip_verified_email';
+
+	const GET_EMAIL_VERIFY  = 'vip_verify_code';
+	const GET_EMAIL_USER_ID = 'vip_user_id';
 
 	protected $reverting_role;
 
@@ -48,6 +55,7 @@ class VipSupportUser {
 		add_action( 'admin_notices',   array( $this, 'action_admin_notices' ) );
 		add_action( 'set_user_role',   array( $this, 'action_set_user_role' ), 10, 3 );
 		add_action( 'user_register',   array( $this, 'action_user_register' ) );
+		add_action( 'parse_request',   array( $this, 'action_parse_request' ) );
 
 		add_filter( 'wp_redirect',     array( $this, 'filter_wp_redirect' ) );
 
@@ -104,6 +112,7 @@ class VipSupportUser {
 		}
 		$user = new WP_User( $user_id );
 		$becoming_support = ( VipSupportRole::VIP_SUPPORT_ROLE == $role );
+//		if ( $becoming_support && ( ! $this->is_a8c_email( $user->user_email ) || ! $this->user_has_verified_email( $user_id ) ) ) {
 		if ( $becoming_support && ! $this->is_a8c_email( $user->user_email ) ) {
 			$this->reverting_role = true;
 			if ( ! is_array( $old_roles ) || ! isset( $old_roles[0] ) ) {
@@ -147,13 +156,46 @@ class VipSupportUser {
 		if ( $this->is_a8c_email( $user->user_email ) ) {
 			$user->set_role( VipSupportRole::VIP_SUPPORT_ROLE );
 			$this->registering_vip = true;
-			update_user_meta( $user_id, 'vip_verified_email', false );
+			update_user_meta( $user_id, self::META_EMAIL_VERIFIED, false );
 			$this->send_verification_email( $user_id );
 		} else {
 			if ( self::MSG_BLOCKED_UPGRADE == $this->message_replace ) {
 				$this->message_replace = self::MSG_BLOCKED_NEW_NON_VIP_USER;
 			}
 		}
+	}
+
+	public function action_parse_request() {
+		if ( ! isset( $_GET[self::GET_EMAIL_VERIFY] ) ) {
+			return;
+		}
+
+		$user_id = absint( $_GET[self::GET_EMAIL_USER_ID] );
+		$user = get_user_by( 'id', $user_id );
+		if ( ! $user ) {
+			return;
+		}
+
+		$stored_verification_code = (string) get_user_meta( $user_id, self::META_VERIFICATION_CODE, true );
+		$hash_sent                = (string) $_GET[self::GET_EMAIL_VERIFY];
+
+		$check_hash = wp_hash( $user_id . $stored_verification_code . $user->user_email );
+
+		if ( $check_hash !== $hash_sent ) {
+			$message = __( 'This email verification link was not recognised, has been invalidated, or has already been used.', 'vip-support' );
+			$title = __( 'Verification failed', 'vip-support' );
+			// 403 Forbidden – The server understood the request, but is refusing to fulfill it.
+			// Authorization will not help and the request SHOULD NOT be repeated.
+			wp_die( $message, $title, array( 'response' => 403 ) );
+		}
+
+		// It's all looking good. Verify the email.
+		update_user_meta( $user_id, self::META_EMAIL_VERIFIED, $user->user_email );
+		delete_user_meta( $user_id, self::META_VERIFICATION_CODE );
+
+		$message = sprintf( __( 'Your email has been verified as %s', 'vip-support' ), $user->user_email );
+		$title = __( 'Verification succeeded', 'vip-support' );
+		wp_die( $message, $title, array( 'response' => 200 ) );
 	}
 
 	// CALLBACKS
@@ -169,7 +211,13 @@ class VipSupportUser {
 			$verification_code = uniqid();
 			update_user_meta( $user_id, self::META_VERIFICATION_CODE, $verification_code );
 		}
-		$verification_link = add_query_arg( array( 'vip_verification' => $verification_code ), home_url() );
+		$user = new WP_User( $user_id );
+		$hash = wp_hash( $user_id . $verification_code . $user->user_email );
+
+		$hash              = urlencode( $hash );
+		$user_id           = absint( $user_id );
+		$verification_link = add_query_arg( array( self::GET_EMAIL_VERIFY => $hash, self::GET_EMAIL_USER_ID => $user_id ), home_url() );
+
 		$user = new WP_User( $user_id );
 
 		$message  = __( 'Dear VIP Support User,', 'vip-support' );
@@ -181,9 +229,6 @@ class VipSupportUser {
 		$message .= __( 'If you have any questions, please contact the WordPress.com VIP Support Team.' );
 
 		$subject = sprintf( __( 'Email verification for %s', 'vip-support' ), get_bloginfo( 'name' ) );
-
-		error_log( "Subject: $subject" );
-		error_log( "Message: $message" );
 
 		wp_mail( $user->user_email, $subject, $message );
 	}
