2b2f39d4b85bb42b713090953109415b2a987067
Simon Wheatley <simon@simonwheatley.co.uk>
2015-05-14 15:58:10 +0100

    Uncertain why this is failing

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -39,6 +39,8 @@ install_wp() {
 	fi
 
 	download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+	stat /tmp/wordpress.tar.gz
+	ls -alh /tmp/
 	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
 	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
