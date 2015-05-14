ba811dee58e049ef372d24492000692d8803ecb8
Simon Wheatley <simon@simonwheatley.co.uk>
2015-05-14 16:46:52 +0100

    Get more of the WordPress PHPUnit suite

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -56,7 +56,7 @@ install_test_suite() {
 	if [ ! "$(ls -A $WP_TESTS_DIR)" ]; then
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
-		svn co --quiet http://develop.svn.wordpress.org/trunk/tests/phpunit/includes/ $WP_TESTS_DIR
+		svn co --quiet http://develop.svn.wordpress.org/trunk/tests/phpunit/ $WP_TESTS_DIR
 	fi
 
 	cd $WP_TESTS_DIR
