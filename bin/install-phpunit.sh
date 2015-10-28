402e957d888b2be03963323dc9f4f823f25d60df
Simon Wheatley <simon@simonwheatley.co.uk>
2015-10-28 15:03:48 +0000

    Switch the test install script to be version specific
    WordPress and the test lib are installed to version
    specific dirs and symlinked in to allow multiple
    versions to be installed (effectively cached) locally.
    
    The script now installs WordPress and the WordPress
    test lib to a version specific directory, in order that
    we don't run into issues where classes or functions
    are not defined in WordPress but are used in the
    test lib.

--- bin/install-phpunit.sh
+++ bin/install-phpunit.sh
@@ -11,24 +11,61 @@ DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 
-WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
+WP_TESTS_DIR="${WP_TESTS_DIR-/tmp/wordpress-tests-lib}/"
+WP_CORE_DIR="${WP_CORE_DIR-/tmp/wordpress}/"
+
+download() {
+    if [ `which curl` ]; then
+        curl -s "$1" > "$2";
+    elif [ `which wget` ]; then
+        wget -nv -O "$2" "$1"
+    fi
+}
+
+if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
+	WP_TESTS_TAG="tags/$WP_VERSION"
+else
+	# http serves a single offer, whereas https serves multiple. we only want one
+	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
+	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
+	WP_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
+	if [[ -z "$WP_VERSION" ]]; then
+		echo "Latest WordPress version could not be found"
+		exit 1
+	fi
+	WP_TESTS_TAG="tags/$WP_VERSION"
+fi
+
 
 set -ex
 
-install_wp() {
-	mkdir -p $WP_CORE_DIR
+# Footle around to remove any trailing slashes, and then add
+# them back in again.
+# We're going to install WordPress and the WordPress test lib
+# to version specific directories, for greater control locally
+shopt -s extglob;
+WP_TESTS_DIR_ACTUAL="${WP_TESTS_DIR%%+(/)}-${WP_VERSION}/"
+WP_CORE_DIR_ACTUAL="${WP_CORE_DIR%%+(/)}-${WP_VERSION}/"
 
-	if [ $WP_VERSION == 'latest' ]; then 
+install_wp() {
+
+	if [ -d $WP_CORE_DIR_ACTUAL ]; then
+		return;
+	fi
+
+	mkdir -p $WP_CORE_DIR_ACTUAL
+
+	if [ $WP_VERSION == 'latest' ]; then
 		local ARCHIVE_NAME='latest'
 	else
 		local ARCHIVE_NAME="wordpress-$WP_VERSION"
 	fi
 
-	wget -nv -O /tmp/wordpress.tar.gz https://wordpress.org/${ARCHIVE_NAME}.tar.gz
-	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+	download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR_ACTUAL
 
-	wget -nv -O $WP_CORE_DIR/wp-content/db.php https://raw.github.com/markoheijnen/wp-mysqli/master/db.php
+	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR_ACTUAL/wp-content/db.php
+
 }
 
 install_test_suite() {
@@ -39,17 +76,24 @@ install_test_suite() {
 		local ioption='-i'
 	fi
 
-	# set up testing suite
-	mkdir -p $WP_TESTS_DIR
-	cd $WP_TESTS_DIR
-	svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/includes/
+	# set up testing suite if it doesn't yet exist
+	if [ ! -d $WP_TESTS_DIR_ACTUAL ]; then
+		# set up testing suite
+		mkdir -p $WP_TESTS_DIR_ACTUAL
+		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR_ACTUAL/includes
+	fi
 
-	wget -nv -O wp-tests-config.php https://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
-	sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
-	sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
-	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
-	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
-	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+	cd $WP_TESTS_DIR_ACTUAL
+
+	if [ ! -f wp-tests-config.php ]; then
+		download https://develop.svn.wordpress.org/${WP_TESTS_TAG}/wp-tests-config-sample.php "$WP_TESTS_DIR_ACTUAL"/wp-tests-config.php
+		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" "$WP_TESTS_DIR_ACTUAL"/wp-tests-config.php
+		sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR_ACTUAL"/wp-tests-config.php
+		sed $ioption "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR_ACTUAL"/wp-tests-config.php
+		sed $ioption "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR_ACTUAL"/wp-tests-config.php
+		sed $ioption "s|localhost|${DB_HOST}|" "$WP_TESTS_DIR_ACTUAL"/wp-tests-config.php
+	fi
+
 }
 
 install_db() {
@@ -75,4 +119,12 @@ install_db() {
 
 install_wp
 install_test_suite
+
+# Create a symbolic link to the version specific directories
+# from a generic location
+rm -rf "${WP_CORE_DIR%%+(/)}"
+rm -rf "${WP_TESTS_DIR%%+(/)}"
+ln -s "${WP_CORE_DIR_ACTUAL%%+(/)}" "${WP_CORE_DIR%%+(/)}"
+ln -s "${WP_TESTS_DIR_ACTUAL%%+(/)}" "${WP_TESTS_DIR%%+(/)}"
+
 install_db
