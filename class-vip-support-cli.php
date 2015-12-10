242ef132a3bb76658419363ff6e0d5764aff31ad
Simon Wheatley <simon@simonwheatley.co.uk>
2015-12-10 16:30:14 +0000

    CLI: Add a command to create a support user
    Sends no verification emails
    Verifies the user after creation
    Generates a random 64 digit password if none is created
    Emails the site admin, because transparency is good

--- class-vip-support-cli.php
+++ class-vip-support-cli.php
@@ -11,6 +11,64 @@ use \WP_User_Query;
  */
 class WPCOM_VIP_Support_CLI  extends WP_CLI_Command {
 
+
+	/**
+	 * Creates a user in the VIP Support role, already verified,
+	 * and suppresses all emails.
+	 *
+	 * @subcommand create-user
+	 *
+	 * @synopsis <user-login> <user-email> [--user-pass=<user-pass>] [--display-name=<display-name>]
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp vipsupport create-user username user@domain.tld display_name
+	 *
+	 */
+	public function add_support_user( $args, $assoc_args ) {
+
+		$user_login   = $args[0];
+		$user_email   = $args[1];
+		$user_pass = $assoc_args['user-pass'];
+		$display_name = $assoc_args['display-name'];
+
+		// @TODO Verify the email address
+		if ( ! is_email( $user_email ) ) {
+			\WP_CLI::error( "User cannot be added as '{$user_email}' is not a valid email address" );
+		}
+
+		if ( empty( $user_pass ) ) {
+			$user_pass = wp_generate_password( 64 );
+		}
+
+		$user_data = array();
+		$user_data['user_pass']    = $user_pass;
+		$user_data['user_login']   = $user_login;
+		$user_data['user_email']   = $user_email;
+		$user_data['display_name'] = $display_name;
+
+		$user_id = wp_insert_user( $user_data );
+
+		// It's possible the user insert will fail, we need to cope with this
+		if ( is_wp_error( $user_id ) ) {
+			\WP_CLI::error( $user_id );
+		}
+		remove_action( 'set_user_role', array( WPCOM_VIP_Support_User::init(), 'action_set_user_role' ), 10, 3 );
+		$user = new WP_User( $user_id );
+		add_action( 'set_user_role', array( WPCOM_VIP_Support_User::init(), 'action_set_user_role' ), 10, 3 );
+
+		// Seems polite to notify the admin that a support user got added to their site
+		// @TODO Tailor the notification email so it explains that this is a support user
+		wp_new_user_notification( $user_id, null, 'admin' );
+
+		WPCOM_VIP_Support_User::init()->mark_user_email_verified( $user->ID, $user->user_email );
+		$user->set_role( WPCOM_VIP_Support_Role::VIP_SUPPORT_ROLE );
+
+		// Print a success message
+		\WP_CLI::success( "Added user $user_id with login {$user_login}, they are verified as a VP Support user and ready to go" );
+	}
+
+
 	/**
 	 * Marks the user with the provided ID as having a verified email.
 	 *
