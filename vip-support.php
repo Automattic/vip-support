8712cdc2d901453b75d239fc05580dfd7a3e6938
Josh Betz <j@joshbetz.com>
2017-06-20 09:54:26 -0500

    Activate rest api endpoint
    Also bump version to 3.1.0

--- vip-support.php
+++ vip-support.php
@@ -2,13 +2,14 @@
 /**
  * Plugin Name:  WordPress.com VIP Support
  * Description:  Manages the WordPress.com Support Users on your site
- * Version:      3.0.0
+ * Version:      3.1.0
  * Author:       <a href="http://automattic.com">Automattic</a>
  * License:      GPLv2 or later
  */
 
 require_once( __DIR__ . '/class-vip-support-role.php' );
 require_once( __DIR__ . '/class-vip-support-user.php' );
+require_once( __DIR__ . '/class-vip-support-api.php' );
 if ( defined('WP_CLI') && WP_CLI ) {
 	require_once( __DIR__ . '/class-vip-support-cli.php' );
 }
